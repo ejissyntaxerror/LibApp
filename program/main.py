ff0f6bb371db2116001572d7b8dd391cce4a3b05
ejissyntaxerror <164166792+ejissyntaxerror@users.noreply.github.com>
2026-09-18 21:28:01 +0800

    Add student ID, time, and appointment purpose

--- program/main.py
+++ program/main.py
@@ -1,8 +1,14 @@
 print("=== LIBRARIAN APPOINTMENT SYSTEM ===")
 
 name = input("Enter your name: ")
+student_id = input("Enter your student ID: ")
 date = input("Enter appointment date: ")
+time = input("Enter appointment time: ")
+purpose = input("Enter purpose of appointment: ")
 
 print("\n=== APPOINTMENT DETAILS ===")
 print("Name:", name)
+print("Student ID:", student_id)
 print("Date:", date)
+print("Time:", time)
+print("Purpose:", purpose)
