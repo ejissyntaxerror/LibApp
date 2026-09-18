59e95c04b7688c0c9edde3b97da269165e3820cd
ejissyntaxerror <164166792+ejissyntaxerror@users.noreply.github.com>
2026-09-18 21:21:55 +0800

    basic Librarian appointment system

---
+++ program/main.py
@@ -0,0 +1,8 @@
+print("=== LIBRARIAN APPOINTMENT SYSTEM ===")
+
+name = input("Enter your name: ")
+date = input("Enter appointment date: ")
+
+print("\n=== APPOINTMENT DETAILS ===")
+print("Name:", name)
+print("Date:", date)
